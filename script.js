b86abf8bbd0d98f6c88d8fe66ca5f9251ca58524
Rahat Siddique <rahatsiddique2717@gmail.com>
2021-11-10 10:20:31 +0600

    after creating function

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 document.getElementById("phone-increase").addEventListener('click', function(){
-    const phoneInput = document.getElementById("phone-count");
-    const phoneCount = parseInt(phoneInput.value);
-    const phoneNewCount = phoneCount + 1;
-    phoneInput.value = phoneNewCount;
-    const phoneTotal = phoneNewCount * 1219;
-    document.getElementById("phone-total").innerText = "$" + phoneTotal;
+    // const phoneInput = document.getElementById("phone-count");
+    // const phoneCount = parseInt(phoneInput.value);
+    // const phoneNewCount = phoneCount + 1;
+    // phoneInput.value = phoneNewCount;
+    // const phoneTotal = phoneNewCount * 1219;
+    // document.getElementById("phone-total").innerText = '$' + phoneTotal;
+    handleProductChange(true);
 });
 
 document.getElementById("phone-decrease").addEventListener('click', function(){
+    // const phoneInput = document.getElementById("phone-count");
+    // const phoneCount = parseInt(phoneInput.value);
+    // const phoneNewCount = phoneCount - 1;
+    // phoneInput.value = phoneNewCount;
+    // const phoneTotal = phoneNewCount * 1219;
+    // document.getElementById("phone-total").innerText = '$' + phoneTotal;
+    handleProductChange(false);
+})
+
+function handleProductChange(isIncrease){
     const phoneInput = document.getElementById("phone-count");
     const phoneCount = parseInt(phoneInput.value);
-    const phoneNewCount = phoneCount - 1;
+    // const phoneNewCount = phoneCount - 1;
+    let phoneNewCount = phoneCount;
+    if(isIncrease == true){
+        phoneNewCount = phoneCount + 1;
+    }
+    if(isIncrease == false && phoneCount > 0){
+        phoneNewCount = phoneCount - 1;
+    }
     phoneInput.value = phoneNewCount;
     const phoneTotal = phoneNewCount * 1219;
-    document.getElementById('phone-total').innerText = '$' + phoneTotal;
-})
+    document.getElementById("phone-total").innerText = '$' + phoneTotal;
+}
